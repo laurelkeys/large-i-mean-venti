eecb9cf311e1d47c9ad6d6d2cfa1ab6d786b5ff8
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-07-09 16:11:00 -0300

    Change particle amount on scroll

--- computer-graphics/halftone/p5js/sketch.js
+++ computer-graphics/halftone/p5js/sketch.js
@@ -9,17 +9,20 @@ function preload() {
 function setup() {
   createCanvas(500, 700, WEBGL);
   setAttributes('antialias', true);
-  
+
   cam = createEasyCam();
   cam.setDistanceMin(300);
   cam.setDistanceMax(300);
-  
+
   img.resize(500, 700);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  cam.setViewport([0,0,windowWidth, windowHeight]);
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    --particles;
+  } else {
+    ++particles;
+  }
 }
 
 function draw() {
@@ -28,17 +31,17 @@ function draw() {
   fill('#111111');
   noStroke();
   ortho();
-  
+
   let particleSize = parseInt(width / particles);
   img.loadPixels();
   for (let y = 0; y < img.height; y += particleSize) {
     for (let x = 0; x < img.width; x += particleSize) {
       let off = (y * img.width + x) * 4;
-      let rgb = img.pixels.slice(off, off+3);
+      let rgb = img.pixels.slice(off, off + 3);
       let darkness = 1 - (rgb.reduce((acc, x) => acc + x) / 3 / 255);
 
       push();
-      let d = darkness <= 0.5 ? (2*darkness)**0.5 : (2*(darkness-0.5))**0.5;
+      let d = darkness <= 0.5 ? (2 * darkness) ** 0.5 : (2 * (darkness - 0.5)) ** 0.5;
       translate(x, y, d * 200);
       box(darkness * particleSize);
       pop();
