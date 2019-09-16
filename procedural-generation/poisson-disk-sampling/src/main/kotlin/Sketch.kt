0bcdf8f5c2aa5fe7c778f868d56888d74188ebd6
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-09-16 11:27:40 -0300

    Finish Poisson Disk Sampling

--- procedural-generation/poisson-disk-sampling/src/main/kotlin/Sketch.kt
+++ procedural-generation/poisson-disk-sampling/src/main/kotlin/Sketch.kt
@@ -1,7 +1,7 @@
 // ref.: https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
 
+import extensions.circle
 import extensions.random
-import extensions.stackMatrix
 import processing.core.PApplet
 import processing.core.PVector
 
@@ -12,12 +12,16 @@ class Sketch(
 ) : PApplet() {
 
     private val cellScale = radius / sqrt(N.toFloat())
-    private val rows by lazy { floor(height / cellScale) }
-    private val columns by lazy { floor(width / cellScale) }
-    private val grid by lazy { arrayOfNulls<PVector>(rows * columns) }
-    private val active = mutableListOf<PVector>()
+
+    private val rows by lazy { ceil(height / cellScale) }
+    private val columns by lazy { ceil(width / cellScale) }
+    private val grid by lazy { IntArray(rows * columns) { NO_POINT } }
+
+    private val points = mutableListOf<PVector>()
+    private val activePoints = mutableListOf<PVector>()
 
     companion object {
+        private const val NO_POINT = -1
         private const val N = 2 // number of dimensions
         fun run(r: Float, k: Int = 30, spf: Int = 4) = Sketch(r, k, spf).runSketch()
     }
@@ -26,7 +30,7 @@ class Sketch(
     //   ___    _
     //  |_|_|..|_|      grid cell:
     //  |_|_|..|_|          _
-    //  :   :  : :  ==>    |_| (cellScale x cellScale)
+    //  :   :  : :  ==>    |_| (1 x 1)
     //  |_|_|..|_|
     private fun inGrid(u: Int, v: Int) = u in 0 until columns && v in 0 until rows
 
@@ -41,68 +45,77 @@ class Sketch(
         val initialSample = PVector(width / 2f, height / 2f) // PVector(random(width), random(height))
         val u = floor(initialSample.x / cellScale)
         val v = floor(initialSample.y / cellScale)
-        grid[u + v * columns] = initialSample
-        active.add(initialSample)
+        grid[u + v * columns] = 0 // first sample's index
+
+        points.add(initialSample)
+        activePoints.add(initialSample)
     }
 
     override fun draw() {
         background(0f)
-        strokeWeight(4f)
+        strokeWeight(3f)
 
         repeat(times = samplesPerFrame) {
-            if (active.isNotEmpty()) {
-                val randomIndex = floor(random(active.size))
-                val activeSample = active[randomIndex]
+            if (activePoints.isNotEmpty()) {
+                val randomIndex = floor(random(activePoints.size))
+                val activeSample = activePoints[randomIndex]
 
-                var foundValidSample = false
+                var candidateAccepted = false
                 for (attempt in 0 until maxSampleAttempts) {
-                    val sample = PVector
+                    val candidate = PVector
                         .random2D()
-                        .setMag(sqrt(random(radius * radius, (2 * radius) * (2 * radius))))
+                        .setMag(sqrt(random(radius * radius, 4f * radius * radius)))
                         .add(activeSample)
 
-                    // sample's location in the grid
-                    val u = floor(sample.x / cellScale)
-                    val v = floor(sample.y / cellScale)
-
-                    // check if the generated sample is new and adequately far from existing samples
-                    if (inGrid(u, v) && grid[u + v * columns] == null) {
-                        var farEnough = true
-                        (-1..1).forEach { i ->
-                            (-1..1).forEach { j ->
-                                val neighbor = grid.getOrNull((u + i) + (v + j) * columns)
-                                if (neighbor != null) {
-                                    val squaredDistance = PVector.sub(sample, neighbor).magSq()
-                                    farEnough = squaredDistance >= radius * radius
-                                }
-                            }
-                        }
-                        if (farEnough) {
-                            foundValidSample = true
-                            grid[u + v * columns] = sample
-                            active.add(sample)
-                            break
-                        }
+                    // candidate's location in the grid
+                    val u = floor(candidate.x / cellScale)
+                    val v = floor(candidate.y / cellScale)
+                    if (isValid(candidate, u, v)) {
+                        points.add(candidate)
+                        activePoints.add(candidate)
+                        grid[u + v * columns] = points.size - 1 // 0 based index of the accepted candidate
+                        candidateAccepted = true
                     }
                 }
 
-                if (!foundValidSample) active.removeAt(randomIndex)
+                if (!candidateAccepted) activePoints.removeAt(randomIndex)
 
-            } else noLoop()
+            } else {
+                // TODO save the points list here if you want to
+                noLoop() // we are finished
+            }
         }
 
         stroke(255f)
-        strokeWeight(4f)
-        for (sample in grid) {
-            if (sample != null) point(sample.x, sample.y)
+        strokeWeight(3f)
+        grid.filter { it != NO_POINT }.forEach {
+            val sample = points[it]
+            point(sample.x, sample.y)
         }
 
         stroke(255f, 0f, 255f)
-        strokeWeight(6f)
-        for (sample in active) {
+        strokeWeight(5f)
+        activePoints.forEach { sample ->
             point(sample.x, sample.y)
         }
     }
+
+    private fun isValid(candidate: PVector, u: Int, v: Int): Boolean {
+        if (inGrid(u, v)) {
+            val delta = 1 // searches for neighbors in a (2*delta+1) x (2*delta+1) region around (u, v)
+            for (t in -delta..delta) {
+                for (s in -delta..delta) {
+                    val neighbor = grid.getOrNull((u + s) + (v + t) * columns) ?: continue
+                    if (neighbor != NO_POINT) {
+                        val squaredDistance = PVector.sub(candidate, points[neighbor]).magSq()
+                        if (squaredDistance < radius * radius) return false
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
 }
 
-fun main() = Sketch.run(r = 24f)
+fun main() = Sketch.run(r = 12f, spf = 6)
