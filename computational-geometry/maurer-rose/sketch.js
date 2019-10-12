bdcee68bf6b65fc8815b5d0a3f6a5905afaf8060
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-10-12 10:53:34 -0300

    WIP: 3D Rose-like object

--- computational-geometry/maurer-rose/sketch.js
+++ computational-geometry/maurer-rose/sketch.js
@@ -7,10 +7,20 @@ let n = 2;
 let d = 29;
 let scale = 150;
 
+const f = 0.000001;
+let time = 0;
+let slider1, slider2, slider3, slider4, slider5;
+
 function setup() {
   createCanvas(bbox, bbox, WEBGL);
   setAttributes('antialias', true);
   cam = createEasyCam();
+  
+  // slider1 = createSlider(1, 180, 1);
+  // slider2 = createSlider(1, 180, 1);
+  // slider3 = createSlider(1, 180, 1);
+  // slider4 = createSlider(1, 180, 1);
+  // slider5 = createSlider(1, 180, 1);
 }
 
 function draw() {
@@ -18,11 +28,35 @@ function draw() {
   drawAxis();
   drawBBox();
   
-  stroke(0);
-  drawMaurerRose(n, d, scale);
-  
-  stroke(255, 0, 255);
-  drawMaurerRose(n, 1, scale); // when d == 1 we have a usual rose (rhodonea curve)
+  // stroke(0);
+  // drawMaurerRose(n, d, scale);
+  // stroke(255, 0, 255);
+  // drawMaurerRose(n, 1, scale); // when d == 1 we have a usual rose (rhodonea curve)
+
+  cam.rotateZ(0.001 * map(noise(f * time++), 0, 1, -2, 1));
+  cam.rotateY(0.01 * map(noise(f * time++), 0, 1, -1, 1));
+  cam.rotateX(0.005 * map(noise(f * time++), 0, 1, -1, 1));
+  draw3DRose(scale,
+             map(noise(f * time++), 0, 1, 1, 180),
+             map(noise(f * time++), 0, 1, 1, 180),
+             map(noise(f * time++), 0, 1, 1, 180),
+             map(noise(f * time++), 0, 1, 1, 180),
+             map(noise(f * time++), 0, 1, 1, 180));
+}
+
+function draw3DRose(scale, m=4, n=1, ds=1, dt=1, ct=0) {
+  noFill();
+  beginShape();
+  for (let angle = 0; angle <= 360; ++angle) {
+    const s = ds * radians(angle);
+    const t = dt * s + ct;
+    const r = scale * cos(m*t) * cos(n*s);
+    const x = r * cos(t) * sin(s);
+    const y = r * sin(t) * sin(s);
+    const z = r * sin(s);
+    vertex(x, y, z);
+  }
+  endShape();
 }
 
 function drawMaurerRose(n, d, scale) {
