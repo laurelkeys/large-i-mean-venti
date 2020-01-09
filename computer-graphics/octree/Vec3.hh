2066926e3c081c766dda354e8de676085dda1528
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-01-09 15:30:42 -0300

    Refactor Vec3

--- computer-graphics/octree/Vec3.hh
+++ computer-graphics/octree/Vec3.hh
@@ -1,72 +1,40 @@
 #ifndef VEC3_HH
 #define VEC3_HH
 
+#include <algorithm>
 #include <cmath>
+
+typedef unsigned int uint;
 
 struct Vec3;
 Vec3 operator*(float r, const Vec3 &v);
 
 struct Vec3 {
     union {
-        struct {
-            float x, y, z;
-        };
+        struct { float x, y, z; };
         float D[3];
     };
 
-    Vec3() {}
-    Vec3(float _x, float _y, float _z) :
-        x(_x), y(_y), z(_z) {}
+    Vec3() { }
+    Vec3(float x, float y, float z)
+        : x(x)
+        , y(y)
+        , z(z) { }
 
-    float &operator[](unsigned int i) {
-        return D[i];
-    }
+    float &operator[](uint i) { return D[i]; }
+    const float &operator[](uint i) const { return D[i]; }
 
-    const float &operator[](unsigned int i) const {
-        return D[i];
-    }
+    float maxComponent() const { return std::max(x, std::max(y, z)); }
+    float minComponent() const { return std::min(x, std::min(y, z)); }
 
-    float maxComponent() const {
-        float r = x;
-        if (y > r)
-            r = y;
-        if (z > r)
-            r = z;
-        return r;
-    }
+    Vec3 operator+(const Vec3 &r) const { return Vec3(x + r.x, y + r.y, z + r.z); }
+    Vec3 operator-(const Vec3 &r) const { return Vec3(x - r.x, y - r.y, z - r.z); }
 
-    float minComponent() const {
-        float r = x;
-        if (y < r)
-            r = y;
-        if (z < r)
-            r = z;
-        return r;
-    }
+    Vec3 cmul(const Vec3 &r) const { return Vec3(x * r.x, y * r.y, z * r.z); }
+    Vec3 cdiv(const Vec3 &r) const { return Vec3(x / r.x, y / r.y, z / r.z); }
 
-    Vec3 operator+(const Vec3 &r) const {
-        return Vec3(x + r.x, y + r.y, z + r.z);
-    }
-
-    Vec3 operator-(const Vec3 &r) const {
-        return Vec3(x - r.x, y - r.y, z - r.z);
-    }
-
-    Vec3 cmul(const Vec3 &r) const {
-        return Vec3(x * r.x, y * r.y, z * r.z);
-    }
-
-    Vec3 cdiv(const Vec3 &r) const {
-        return Vec3(x / r.x, y / r.y, z / r.z);
-    }
-
-    Vec3 operator*(float r) const {
-        return Vec3(x * r, y * r, z * r);
-    }
-
-    Vec3 operator/(float r) const {
-        return Vec3(x / r, y / r, z / r);
-    }
+    Vec3 operator*(float r) const { return Vec3(x * r, y * r, z * r); }
+    Vec3 operator/(float r) const { return Vec3(x / r, y / r, z / r); }
 
     Vec3 &operator+=(const Vec3 &r) {
         x += r.x;
@@ -89,29 +57,23 @@ struct Vec3 {
         return *this;
     }
 
+    float norm() const { return sqrtf(x * x + y * y + z * z); }
+    float normSquared() const { return x * x + y * y + z * z; }
+
+    Vec3 normalized() const {
+        return *this / norm();
+    }
+
     // Inner/dot product
     float operator*(const Vec3 &r) const {
         return x * r.x + y * r.y + z * r.z;
     }
 
-    float norm() const {
-        return sqrtf(x * x + y * y + z * z);
-    }
-
-    float normSquared() const {
-        return x * x + y * y + z * z;
-    }
-
     // Cross product
     Vec3 operator^(const Vec3 &r) const {
-        return Vec3(
-            y * r.z - z * r.y,
-            z * r.x - x * r.z,
-            x * r.y - y * r.x);
-    }
-
-    Vec3 normalized() const {
-        return *this / norm();
+        return Vec3(y * r.z - z * r.y,
+                    z * r.x - x * r.z,
+                    x * r.y - y * r.x);
     }
 };
 
